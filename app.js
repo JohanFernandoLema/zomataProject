763161d129938fa2f4c57d3e7c8d9a6b763c5b4b
JohanFernandoLema <johan.13fernando@gmail.com>
2023-05-13 13:49:32 -0400

    arry of objects created in the js file in ot=rder to call them dynamicaly in our html file

---
+++ app.js
@@ -0,0 +1,38 @@
+const meals = [
+  {
+    id: 1,
+    category: 'breakfast',
+    img: './images/shutterstock_1154073754@2x.png',
+    desc: 'Start your day with exclusive breakfast options',
+  },
+  {
+    id: 2,
+    category: 'lunch',
+    img: './images/shutterstock_1130181932@2x.png',
+    desc: 'Start your day with exclusive breakfast options',
+  },
+  {
+    id: 3,
+    category: 'snacks',
+    img: './images/shutterstock_476864884@2x.png',
+    desc: 'Start your day with exclusive breakfast options',
+  },
+  {
+    id: 4,
+    category: 'dinner',
+    img: './images/shutterstock_476864884@2x.png',
+    desc: 'Start your day with exclusive breakfast options',
+  },
+  {
+    id: 5,
+    category: 'drinks',
+    img: './images/shutterstock_476864884@2x.png',
+    desc: 'Start your day with exclusive breakfast options',
+  },
+  {
+    id: 6,
+    category: 'nightlife',
+    img: './images/shutterstock_1304064250@2x.png',
+    desc: 'Start your day with exclusive breakfast options',
+  },
+]
