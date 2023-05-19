60ee748c234f6d3b4812763e612c846e7bec965a
JohanFernandoLema <johan.13fernando@gmail.com>
2023-05-19 08:34:51 -0400

    new layout for categories added using bootstrap and css

--- app.js
+++ app.js
@@ -5,24 +5,24 @@ const meals = [
     img: './images/shutterstock_1154073754@2x.png',
     desc: 'Start your day with exclusive breakfast options',
   },
-  //   {
-  //     id: 2,
-  //     category: 'lunch',
-  //     img: './images/shutterstock_1130181932@2x.png',
-  //     desc: 'Start your day with exclusive breakfast options',
-  //   },
-  //   {
-  //     id: 3,
-  //     category: 'snacks',
-  //     img: './images/shutterstock_476864884@2x.png',
-  //     desc: 'Start your day with exclusive breakfast options',
-  //   },
-  //   {
-  //     id: 4,
-  //     category: 'dinner',
-  //     img: './images/shutterstock_476864884@2x.png',
-  //     desc: 'Start your day with exclusive breakfast options',
-  //   },
+  // {
+  //   id: 2,
+  //   category: 'lunch',
+  //   img: './images/shutterstock_1130181932@2x.png',
+  //   desc: 'Start your day with exclusive breakfast options',
+  // },
+  // {
+  //   id: 3,
+  //   category: 'snacks',
+  //   img: './images/shutterstock_476864884@2x.png',
+  //   desc: 'Start your day with exclusive breakfast options',
+  // },
+  // {
+  //   id: 4,
+  //   category: 'dinner',
+  //   img: './images/shutterstock_476864884@2x.png',
+  //   desc: 'Start your day with exclusive breakfast options',
+  // },
   //   {
   //     id: 5,
   //     category: 'drinks',
