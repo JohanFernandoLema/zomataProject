fda1a607f255dc6fb140ef0d0e8a00f40b2437ea
JohanFernandoLema <johan.13fernando@gmail.com>
2023-05-19 09:43:43 -0400

    array of objects with meals added dynamically to the html

--- app.js
+++ app.js
@@ -5,53 +5,57 @@ const meals = [
     img: './images/shutterstock_1154073754@2x.png',
     desc: 'Start your day with exclusive breakfast options',
   },
-  // {
-  //   id: 2,
-  //   category: 'lunch',
-  //   img: './images/shutterstock_1130181932@2x.png',
-  //   desc: 'Start your day with exclusive breakfast options',
-  // },
-  // {
-  //   id: 3,
-  //   category: 'snacks',
-  //   img: './images/shutterstock_476864884@2x.png',
-  //   desc: 'Start your day with exclusive breakfast options',
-  // },
-  // {
-  //   id: 4,
-  //   category: 'dinner',
-  //   img: './images/shutterstock_476864884@2x.png',
-  //   desc: 'Start your day with exclusive breakfast options',
-  // },
-  //   {
-  //     id: 5,
-  //     category: 'drinks',
-  //     img: './images/shutterstock_476864884@2x.png',
-  //     desc: 'Start your day with exclusive breakfast options',
-  //   },
-  //   {
-  //     id: 6,
-  //     category: 'nightlife',
-  //     img: './images/shutterstock_1304064250@2x.png',
-  //     desc: 'Start your day with exclusive breakfast options',
-  //   },
+  {
+    id: 2,
+    category: 'lunch',
+    img: './images/shutterstock_1130181932@2x.png',
+    desc: 'Start your day with exclusive breakfast options',
+  },
+  {
+    id: 3,
+    category: 'snacks',
+    img: './images/shutterstock_476864884@2x.png',
+    desc: 'Start your day with exclusive breakfast options',
+  },
+  {
+    id: 4,
+    category: 'dinner',
+    img: './images/shutterstock_476864884@2x.png',
+    desc: 'Start your day with exclusive breakfast options',
+  },
+  {
+    id: 5,
+    category: 'drinks',
+    img: './images/shutterstock_476864884@2x.png',
+    desc: 'Start your day with exclusive breakfast options',
+  },
+  {
+    id: 6,
+    category: 'nightlife',
+    img: './images/shutterstock_1304064250@2x.png',
+    desc: 'Start your day with exclusive breakfast options',
+  },
 ]
 
 const mainContent = document.querySelector('.mainContent')
 
 window.addEventListener('DOMContentLoaded', () => {
-  let loadMeals = meals.map((meal) => {
-    return `<img
-            src=${meal.img} alt=${meal.category}
-            class="galery me-3 p-0"
+  let mealsList = meals.map((list) => {
+    return `<div class="d-flex flex-row cards mb-5">
+          <img
+            src=${list.img}
+            alt=${list.category}
+            class="galery me-2"
           />
-          <div class="text-center">
-            <h3 class="fw-bold">Breakfast</h3>
-            <p class="">
-              ${meal.desc}
-            </p>
-          </div>`
+          <div>
+            <div class="mt-5 d-inline-block m-0 ps-2 pe-2">
+              <h3 class="fw-bold">${list.category}</h3>
+              <p>${list.desc}</p>
+            </div>
+          </div>
+        </div>`
   })
-  loadMeals = loadMeals.join('')
-  mainContent.innerHTML = loadMeals
+  console.log(mealsList)
+  mealsList = mealsList.join('')
+  mainContent.innerHTML = mealsList
 })
